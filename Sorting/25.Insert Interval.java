4445e3e68676852cf6628f96c40dd00057df6ecc
ShajithaS <138300841+ShajithaS@users.noreply.github.com>
2024-12-09 20:37:10 +0530

    Create 25.Insert Interval.java

---
+++ Sorting/25.Insert Interval.java
@@ -0,0 +1,45 @@
+/*
+Geek has an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith event and intervals is sorted in ascending order by starti. He wants to add a new interval newInterval= [newStart, newEnd] where newStart and newEnd represent the start and end of this interval.
+
+Help Geek to insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Examples:
+
+Input: intervals = [[1,3], [4,5], [6,7], [8,10]], newInterval = [5,6]
+Output: [[1,3], [4,7], [8,10]]
+Explanation: The newInterval [5,6] overlaps with [4,5] and [6,7].
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,9]
+Output: [[1,2], [3,10], [12,16]]
+Explanation: The new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+*/
+class Solution {
+    static ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> ans = new ArrayList<>();
+        int n = intervals.length;
+        int i = 0;
+
+        // Add intervals that come before the new interval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            ans.add(intervals[i]);
+            i++;
+        }
+
+        // Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+
+        // Add the merged interval
+        ans.add(newInterval);
+
+        // Add intervals that come after the new interval
+        while (i < n) {
+            ans.add(intervals[i]);
+            i++;
+        }
+
+        return ans;
+    }
+}
