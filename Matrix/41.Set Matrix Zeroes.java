59d610e7c021f8cf2d24bd8f1ff32d1269d16256
ShajithaS <138300841+ShajithaS@users.noreply.github.com>
2024-12-25 12:56:08 +0530

    Create 41.Set Matrix Zeroes.java

---
+++ Matrix/41.Set Matrix Zeroes.java
@@ -0,0 +1,42 @@
+/*
+You are given a 2D matrix mat[][] of size n×m. The task is to modify the matrix such that if mat[i][j] is 0, all the elements in the i-th row and j-th column are set to 0 and do it in constant space complexity.
+
+Examples:
+
+Input: mat[][] = [[1, -1, 1],
+                [-1, 0, 1],
+                [1, -1, 1]]
+Output: [[1, 0, 1],
+        [0, 0, 0],
+        [1, 0, 1]]
+Explanation: mat[1][1] = 0, so all elements in row 1 and column 1 are updated to zeroes.
+Input: mat[][] = [[0, 1, 2, 0],
+                [3, 4, 5, 2],
+                [1, 3, 1, 5]]
+Output: [[0, 0, 0, 0],
+        [0, 4, 5, 0],
+        [0, 3, 1, 0]]
+Explanation: mat[0][0] and mat[0][3] are 0s, so all elements in row 0, column 0 and column 3 are updated to zeroes.
+*/
+class Solution {
+    public void setMatrixZeroes(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        boolean[] rows = new boolean[n];
+        boolean[] cols = new boolean[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows[i] || cols[j]) {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+}
